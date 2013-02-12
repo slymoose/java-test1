33998dab8fe019ad96339a1673cc7ac8c24d1e55
slymoose <jmoorse@gmail.com>
2013-02-12 12:19:01 -0800

    BeerSong 2

--- BeerSong.java
+++ BeerSong.java
@@ -4,17 +4,21 @@ public class BeerSong {
             String word = "bottles";
 
             while (beerNum > 0){
-                if (beerNum == 1){
-                    word = "bottle";
-                }
-
+                
                 System.out.println(beerNum + " " + word + " of beer on the wall");
                 System.out.println(beerNum + " " + word + " of beer");
-                System.out.println("take one down, passit around");
+                System.out.println("Take one down.");
+                System.out.println("Pass it around.");
                 beerNum -= 1;
+                
+                if (beerNum == 1){
+                   word = "bottle";
+                }
+
 
                 if(beerNum > 0){
                     System.out.println(beerNum + " " + word + " of beer on the wall");
+                    System.out.println("");
                 } else {
                     System.out.println("No more bottles of beer on the wall :(");
                 } //end else
